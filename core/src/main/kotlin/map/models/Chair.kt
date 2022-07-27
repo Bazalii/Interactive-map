4d17dd7fed56f14b4f79be8abda5e30428a1542b
t1mtg <timagurman@gmail.com>
2022-07-27 20:45:53 +0300

    fix: renamed field in class Chair

--- core/src/main/kotlin/map/models/Chair.kt
+++ core/src/main/kotlin/map/models/Chair.kt
@@ -5,7 +5,7 @@ import java.util.*
 data class Chair(
     val coordinateX: Double,
     val coordinateY: Double,
-    val chairLegsAmount: Int,
+    val legsAmount: Int,
     var height: Int,
     val creator: String,
     val id: UUID
