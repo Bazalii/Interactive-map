e056aadbb2e0e8ccc8dcc641cb1d0470d588cc91
t1mtg <timagurman@gmail.com>
2022-07-27 14:05:31 +0300

    feat: IMapRepository.kt, Chair.kt, Wall.kt -Add new entities: Chair and Wall -Add methods in IMapRepository

--- core/src/main/kotlin/map/repositories/IMapRepository.kt
+++ core/src/main/kotlin/map/repositories/IMapRepository.kt
@@ -1,4 +1,22 @@
 package map.repositories
 
+import map.models.Chair
+import map.models.Table
+import map.models.Wall
+import java.util.UUID
+
 interface IMapRepository {
+    fun saveTable(table: Table)
+    fun saveChair(chair: Chair)
+    fun saveWall(wall: Wall)
+    fun deleteTableById(tableId: UUID)
+    fun deleteChairById(chairId: UUID)
+    fun deleteWallById(wallId: UUID)
+    fun getTableById(tableId: UUID): Table
+    fun getChairById(chairId: UUID): Chair
+    fun getWallById(wallId: UUID): Wall
+    fun getAllTables(): List<Table>
+    fun getAllChairs(): List<Chair>
+    fun getAllWalls(): List<Wall>
+
 }
