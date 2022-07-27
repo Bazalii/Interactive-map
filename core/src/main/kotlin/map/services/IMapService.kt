99a8952685d614d05b768dc3172c42c515f20a99
FuchsbauFox <vovasai@mail.ru>
2022-07-27 17:35:02 +0300

    feat: MapService.kt, IMapService.kt - add methods in IMapService - add basic MapService implementation

--- core/src/main/kotlin/map/services/IMapService.kt
+++ core/src/main/kotlin/map/services/IMapService.kt
@@ -1,4 +1,21 @@
 package map.services
 
+import map.models.Chair
+import map.models.Table
+import map.models.Wall
+import java.util.*
+
 interface IMapService {
+    fun saveTable(table: Table)
+    fun saveChair(chair: Chair)
+    fun saveWall(wall: Wall)
+    fun deleteTableById(tableId: UUID)
+    fun deleteChairById(chairId: UUID)
+    fun deleteWallById(wallId: UUID)
+    fun getTableById(tableId: UUID): Table
+    fun getChairById(chairId: UUID): Chair
+    fun getWallById(wallId: UUID): Wall
+    fun getAllTables(): List<Table>
+    fun getAllChairs(): List<Chair>
+    fun getAllWalls(): List<Wall>
 }
--- core/src/main/kotlin/map/services/implementations/MapService.kt
+++ core/src/main/kotlin/map/services/implementations/MapService.kt
@@ -1,6 +1,64 @@
 package map.services.implementations
 
+import map.models.Chair
+import map.models.Table
+import map.models.Wall
+import map.repositories.IMapRepository
 import map.services.IMapService
+import java.util.*
+import javax.enterprise.context.ApplicationScoped
 
-class MapService : IMapService {
+@ApplicationScoped
+class MapService(
+    private val repository: IMapRepository,
+) : IMapService {
+
+
+    override fun saveTable(table: Table) {
+        repository.saveTable(table)
+    }
+
+    override fun saveChair(chair: Chair) {
+        repository.saveChair(chair)
+    }
+
+    override fun saveWall(wall: Wall) {
+        repository.saveWall(wall)
+    }
+
+    override fun deleteTableById(tableId: UUID) {
+        repository.deleteTableById(tableId)
+    }
+
+    override fun deleteChairById(chairId: UUID) {
+        repository.deleteChairById(chairId)
+    }
+
+    override fun deleteWallById(wallId: UUID) {
+        repository.deleteWallById(wallId)
+    }
+
+    override fun getTableById(tableId: UUID): Table {
+        return repository.getTableById(tableId)
+    }
+
+    override fun getChairById(chairId: UUID): Chair {
+        return repository.getChairById(chairId)
+    }
+
+    override fun getWallById(wallId: UUID): Wall {
+        return repository.getWallById(wallId)
+    }
+
+    override fun getAllTables(): List<Table> {
+        return repository.getAllTables()
+    }
+
+    override fun getAllChairs(): List<Chair> {
+        return repository.getAllChairs()
+    }
+
+    override fun getAllWalls(): List<Wall> {
+        return repository.getAllWalls()
+    }
 }
