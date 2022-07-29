31dd8788a1262f5b16a19ad044b1e000ba38fcf5
Iv4nov_Alexey <varanous@gmail.com>
2022-07-29 17:03:06 +0000

    Upload New File

---
+++ User.kt
@@ -0,0 +1,11 @@
+package users.models
+
+import java.util.UUID
+
+data class User(
+    val name: String,
+    val surname: String,
+    var nickname: String,
+    var role: Role = Role.USER,
+    val id: UUID
+)
