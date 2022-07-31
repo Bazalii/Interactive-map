d202473f33bc5a16cfda843c22ecb43f949d7746
Iv4nov_Alexey <varanous@gmail.com>
2022-07-31 18:10:12 +0000

    feat: add user controller

--- web/src/main/kotlin/users/controllers/UserController.kt
+++ web/src/main/kotlin/users/controllers/UserController.kt
@@ -1,10 +1,52 @@
 package users.controllers
 
-import org.eclipse.microprofile.graphql.GraphQLApi
+import users.models.User
 import users.services.IUserService
-import javax.enterprise.context.ApplicationScoped
+import org.eclipse.microprofile.graphql.*
+import java.util.*
 
 @GraphQLApi
-@ApplicationScoped
 class UserController(private val _userService: IUserService) {
+    @Query
+    fun getUser(id: UUID): User? {
+        return _userService.find(id)
+    }
+
+    @Query
+    fun getUsers(): List<User>? {
+        return _userService.findAll()
+    }
+
+    @Mutation
+    fun addUser(user: User) {
+        _userService.save(user)
+    }
+
+    @Mutation
+    fun deleteUser(id: UUID) {
+        _userService.delete(id)
+    }
+
+    @Mutation
+    fun createSuperUser(superUser: User) {
+        _userService.createSuperUser(superUser)
+    }
+
+    @Mutation
+    fun deleteSuperUser(id: UUID) {
+        _userService.deleteSuperUserById(id)
+    }
+
+    @Mutation
+    fun updateUserNickname(id: UUID, nickname: String) {
+        val user: User = _userService.find(id)!!
+        _userService.changeNickname(user, nickname)
+    }
+
+    @Mutation
+    fun updateUserRole(changedUserId: UUID, superUserId: UUID) {
+        val changedUser: User = _userService.find(changedUserId)!!
+        val superUser: User = _userService.find(superUserId)!!
+        _userService.changeRole(changedUser, superUser)
+    }
 }
