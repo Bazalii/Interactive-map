fc4d327e22ce37dd4d47b6f8384e566c5b39dc8d
Iv4nov_Alexey <varanous@gmail.com>
2022-08-02 16:12:20 +0000

    feat: add access controls - Add necessary annotations - Add access roles to the corresponding methods - Change the arguments of the last two methods

--- web/src/main/kotlin/users/controllers/UserController.kt
+++ web/src/main/kotlin/users/controllers/UserController.kt
@@ -4,51 +4,64 @@ import users.models.User
 import users.services.IUserService
 import org.eclipse.microprofile.graphql.*
 import java.util.*
+import javax.annotation.security.*
+import javax.enterprise.context.RequestScoped
+import javax.inject.Inject
 
 @GraphQLApi
-class UserController(private val _userService: IUserService) {
+@RequestScoped
+class UserController(@Inject private val _userService: IUserService) {
+    @RolesAllowed("**")
     @Query
-    fun getById(id: UUID): User? {
+    fun getById(id: UUID): User {
         return _userService.find(id)
     }
 
+    @RolesAllowed("**")
     @Query
     fun getAll(): List<User>? {
         return _userService.findAll()
     }
 
+    @RolesAllowed("**")
     @Mutation
-    fun add(user: User) {
+    fun add(user: User): User {
         _userService.save(user)
+        return user
     }
 
+    @RolesAllowed("ADMIN", "SUPERUSER")
     @Mutation
-    fun deleteById(id: UUID) {
+    fun deleteById(id: UUID): User {
         _userService.delete(id)
+        return _userService.find(id)
     }
 
+    @RolesAllowed("SUPERUSER")
     @Mutation
-    fun createSuperUser(superUser: User) {
+    fun createSuperUser(superUser: User): User {
         _userService.createSuperUser(superUser)
+        return superUser
     }
 
+    @RolesAllowed("SUPERUSER")
     @Mutation
-    fun deleteSuperUserById(id: UUID) {
+    fun deleteSuperUserById(id: UUID): User {
         _userService.deleteSuperUserById(id)
+        return _userService.find(id)
     }
 
+    @RolesAllowed("USER", "SUPERUSER")
     @Mutation
-    fun updateNickname(id: UUID, nickname: String) {
-        val user: User = _userService.find(id)!!
-
+    fun updateNickname(user: User, nickname: String): User {
         _userService.changeNickname(user, nickname)
+        return user
     }
 
+    @RolesAllowed("SUPERUSER")
     @Mutation
-    fun updateRole(changedUserId: UUID, superUserId: UUID) {
-        val changedUser: User = _userService.find(changedUserId)!!
-        val superUser: User = _userService.find(superUserId)!!
-
+    fun updateRole(changedUser: User, superUser: User): User {
         _userService.changeRole(changedUser, superUser)
+        return changedUser
     }
 }
