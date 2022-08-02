30d9c161e855ac7786df41b5683307a9d1a9e705
Iv4nov_Alexey <varanous@gmail.com>
2022-08-02 16:06:58 +0000

    feat: add a new field "password"

--- data/src/main/kotlin/users/models/UserDbModel.kt
+++ data/src/main/kotlin/users/models/UserDbModel.kt
@@ -10,6 +10,7 @@ class UserDbModel(
     var name: String = "",
     var surname: String = "",
     var nickname: String = "",
+    var password: String = "",
     var role: Role = Role.USER,
     @Id var id: UUID = UUID.randomUUID()
 )
