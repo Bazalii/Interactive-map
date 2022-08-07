f58ce37dfd747011897623442cfe3873a36d2230
Iv4nov_Alexey <varanous@gmail.com>
2022-08-07 22:33:54 +0000

    feat: add necessary security annotations for authentication

--- data/src/main/kotlin/users/models/UserDbModel.kt
+++ data/src/main/kotlin/users/models/UserDbModel.kt
@@ -1,6 +1,9 @@
 package users.models
 
+import io.quarkus.security.jpa.Password
+import io.quarkus.security.jpa.Username
 import java.util.*
+import javax.json.bind.annotation.JsonbTransient
 import javax.persistence.Entity
 import javax.persistence.Id
 
@@ -9,7 +12,10 @@ import javax.persistence.Id
 class UserDbModel(
     var name: String = "",
     var surname: String = "",
+    @Username
     var nickname: String = "",
+    @Password
+    @JsonbTransient
     var password: String = "",
     var role: Role = Role.USER,
     @Id var id: UUID = UUID.randomUUID()
