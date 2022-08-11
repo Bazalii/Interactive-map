1e67ae2d400288612913f8aadb0a441723f80f88
FuchsbauFox <vovasai@mail.ru>
2022-08-11 14:56:34 +0300

    fix: add RequestScoped annotation

--- web/src/main/kotlin/map/controllers/MapController.kt
+++ web/src/main/kotlin/map/controllers/MapController.kt
@@ -8,8 +8,10 @@ import org.eclipse.microprofile.graphql.GraphQLApi
 import org.eclipse.microprofile.graphql.Mutation
 import org.eclipse.microprofile.graphql.Query
 import java.util.UUID
+import javax.enterprise.context.RequestScoped
 
 @GraphQLApi
+@RequestScoped
 class MapController(private val _mapService: IMapService) {
     @Mutation
     fun saveTable(table: Table) : Table {
