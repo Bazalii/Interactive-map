b86ae90d5f420d871b6da51e57f557044527269f
FuchsbauFox <vovasai@mail.ru>
2022-08-13 13:50:38 +0300

    fix: add assert in MapControllerHttpTests

--- tests/src/test/kotlin/map/MapControllerHttpTests.kt
+++ tests/src/test/kotlin/map/MapControllerHttpTests.kt
@@ -5,6 +5,7 @@ import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import io.restassured.response.Response
 import map.dto.*
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -43,138 +44,202 @@ class MapControllerHttpTests {
         return UUID.fromString(response.substringAfter("\"id\": \"").substringBefore('\"'))
     }
 
+    private fun getStatusResponse(response: String): String {
+        return response.substringAfter('\"').substringBefore('\"')
+    }
+
     @Test
     fun saveChair_SuccessPath_ChairCreatedAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val chairRequest = ChairRequest(0.0, 0.0, "Fox", 2, 4)
         val file = generateStringFromResource(EndPoints.saveChair)
         val query = GraphQlQuery(file, Variables(chairRequest))
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun saveTable_SuccessPath_TableCreatedAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val tableRequest = TableRequest(0.0, 0.0, "Fox", 1, 2, "red")
         val file = generateStringFromResource(EndPoints.saveTable)
         val query = GraphQlQuery(file, Variables(tableRequest))
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun saveWall_SuccessPath_WallCreatedAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val wallRequest = WallRequest(0.0, 0.0, "Fox", 4, 5)
         val file = generateStringFromResource(EndPoints.saveWall)
         val query = GraphQlQuery(file, Variables(wallRequest))
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun deleteChair_SuccessPath_ChairDeletedAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val chairRequest = ChairRequest(0.0, 0.0,  "Fox", 2, 4)
         var file = generateStringFromResource(EndPoints.saveChair)
         var query = GraphQlQuery(file, Variables(chairRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.deleteChair)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun deleteTable_SuccessPath_TableDeletedAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val tableRequest = TableRequest(0.0, 0.0, "Fox", 1, 2, "red")
         var file = generateStringFromResource(EndPoints.saveTable)
         var query = GraphQlQuery(file, Variables(tableRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.deleteTable)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun deleteWall_SuccessPath_WallDeletedAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val wallRequest = WallRequest(0.0, 0.0, "Fox", 4, 5)
         var file = generateStringFromResource(EndPoints.saveWall)
         var query = GraphQlQuery(file, Variables(wallRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.deleteWall)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getChairById_SuccessPath_ChairFoundAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val chairRequest = ChairRequest(0.0, 0.0, "Fox", 2, 4)
         var file = generateStringFromResource(EndPoints.saveChair)
         var query = GraphQlQuery(file, Variables(chairRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.chairById)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getTableById_SuccessPath_TableFoundAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val tableRequest = TableRequest(0.0, 0.0, "Fox", 1, 2, "red")
         var file = generateStringFromResource(EndPoints.saveTable)
         var query = GraphQlQuery(file, Variables(tableRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.tableById)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getWallById_SuccessPath_WallFoundAndReturnedCorrectStatusCode() {
+        // ARRANGE
         val wallRequest = WallRequest(0.0, 0.0, "Fox", 4, 5)
         var file = generateStringFromResource(EndPoints.saveWall)
         var query = GraphQlQuery(file, Variables(wallRequest))
 
+        // ACT
         val id = getIdFromResponse(requestWithoutStatusCode(query).prettyPrint())
 
         file = generateStringFromResource(EndPoints.wallById)
         query = GraphQlQuery(file, Variables(id))
 
-        request(query).prettyPrint()
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getAllChairs_SuccessPath_AllChairsFoundAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val file = generateStringFromResource(EndPoints.chairs)
         val query = GraphQlQuery(file)
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getAllTables_SuccessPath_AllTablesFoundAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val file = generateStringFromResource(EndPoints.tables)
         val query = GraphQlQuery(file)
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 
     @Test
     fun getAllWalls_SuccessPath_AllWallsFoundAndCorrectStatusCodeIsReturned() {
+        // ARRANGE
         val file = generateStringFromResource(EndPoints.walls)
         val query = GraphQlQuery(file)
 
-        request(query).prettyPrint()
+        // ACT
+        val response = request(query).prettyPrint()
+
+        // ASSERT
+        Assertions.assertEquals("data", getStatusResponse(response))
     }
 }
